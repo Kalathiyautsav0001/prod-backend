4e9762680d1647fa868e75de835b5515abddffaf
Mrutsav98 <utsavkalathiya98@gmail.com>
2026-03-04 19:50:09 +0530

    add frontend url in env and change cors

--- server.js
+++ server.js
@@ -10,7 +10,10 @@ require('dotenv').config();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true
+  }));
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
